e3a8326f131f77957b01f1d0996c61c664e6b78e
bodeych <bodeych@gmail.com>
2023-12-04 21:11:33 +0100

    first commit

---
+++ Task.SerializingToJSON/Lad.cs
@@ -0,0 +1,11 @@
+using Newtonsoft.Json;
+
+namespace Task.SerializingToJSON;
+
+
+    public class Lad
+    {
+        public Guid Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
---
+++ Task.SerializingToJSON/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json;
+using Task.SerializingToJSON;
+
+var obj = new Lad()
+{
+    firstName = "Ofsdaf",
+    lastName = "sdfsdf",
+    Id = Guid.NewGuid()
+};
+var jsonString = JsonConvert.SerializeObject(obj, Formatting.Indented);
+
+Console.WriteLine(jsonString);
+var namesText = File.ReadAllText(@"/Users/bodeych/RiderProjects/Task.SerializingToJSON/Task.SerializingToJSON//Names.json");
+var newObj = Newtonsoft.Json.JsonConvert.DeserializeObject<Lad>(namesText);
+
+Console.WriteLine(newObj.firstName);
+Console.ReadLine();
